1b249beec825c72eabd0345767f4f06180ad8bf3
bobbercheng <32662337+bobbercheng@users.noreply.github.com>
2024-04-10 11:01:01 -0400

    update setup

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='hashing',
-    version='0.1',
-    packages=find_packages(),
+    author='Bobber Cheng',
+    author_email = "bobbercheng@hotmail.com",
+    description = "Python SHA256 libary equivalent to Guava - Google Core Libraries for Java",
+    version='0.1.1',
+    packages=['hashing'],
     install_requires=[],
     test_suite='tests'
 )
